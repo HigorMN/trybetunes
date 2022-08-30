db5aa6fece3849e7b8b349a121d2fded927d3a8e
Higor Maranhão <higor.maranhao2000@gmail.com>
2022-08-30 15:41:15 -0300

    botão e input criado

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,38 @@
 import React, { Component } from 'react';
 
 export default class Login extends Component {
+  state = {
+    name: '',
+    buttonDisabled: true,
+  };
+
+  handlechange = ({ taget }) => {
+
+  };
+
   render() {
+    const { name, buttonDisabled } = this.state;
     return (
-      <div data-testid="page-login">Login</div>
+      <div data-testid="page-login">
+        <label htmlFor="login">
+          <input
+            type="text"
+            value={ name }
+            name="login"
+            id="login"
+            data-testid="login-name-input"
+            onChange={ this.handlechange }
+          />
+        </label>
+        <button
+          type="button"
+          data-testid="login-submit-button"
+          disabled
+        >
+          Entrar
+
+        </button>
+      </div>
     );
   }
 }
