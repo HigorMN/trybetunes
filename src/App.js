96a7691b858d9d03fccdcf34919ac1a0e48ddada
Higor Maranhão <higor.maranhao2000@gmail.com>
2022-08-30 15:14:58 -0300

    Pages Profile criado

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Switch } from 'react-router-dom';
 import Album from './pages/Album';
 import Favorites from './pages/Favorites';
 import Login from './pages/Login';
+import Profile from './pages/Profile';
 import Search from './pages/Search';
 
 class App extends React.Component {
@@ -13,6 +14,7 @@ class App extends React.Component {
         <Route exact path="/search" component={ Search } />
         <Route exact path="/album/:id" component={ Album } />
         <Route exact path="/favorites" component={ Favorites } />
+        <Route exact path="/profile" component={ Profile } />
       </Switch>
     );
   }
