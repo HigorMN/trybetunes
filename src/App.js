ee45d6153480c21493a78f1408ce7d51468ca0f1
Higor Maranhão <higor.maranhao2000@gmail.com>
2022-08-30 15:13:35 -0300

    Pages Favorites criado

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Album from './pages/Album';
+import Favorites from './pages/Favorites';
 import Login from './pages/Login';
 import Search from './pages/Search';
 
@@ -11,6 +12,7 @@ class App extends React.Component {
         <Route exact path="/" component={ Login } />
         <Route exact path="/search" component={ Search } />
         <Route exact path="/album/:id" component={ Album } />
+        <Route exact path="/favorites" component={ Favorites } />
       </Switch>
     );
   }
