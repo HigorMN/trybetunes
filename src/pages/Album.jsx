d6daaf56baad8a14190a4868940e56d7a16f07c3
Higor Maranhão <higor.maranhao2000@gmail.com>
2022-08-30 20:48:09 -0300

    Proptypes adicionado

--- src/pages/Album.jsx
+++ src/pages/Album.jsx
@@ -1,9 +1,24 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import getMusics from '../services/musicsAPI';
 
 export default class Album extends Component {
+  componentDidMount() {
+    const { match: { params: { id } } } = this.props;
+    const music = getMusics(id);
+  }
+
   render() {
     return (
       <div data-testid="page-album">Album</div>
     );
   }
 }
+
+Album.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string,
+    }),
+  }),
+}.isRequired;
