bef8ec728b8e189544b35a26a4173bc54a743e9b
Higor Maranhão <higor.maranhao2000@gmail.com>
2022-08-30 15:11:47 -0300

    Pages Album criado

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
+import Album from './pages/Album';
 import Login from './pages/Login';
 import Search from './pages/Search';
 
@@ -9,6 +10,7 @@ class App extends React.Component {
       <Switch>
         <Route exact path="/" component={ Login } />
         <Route exact path="/search" component={ Search } />
+        <Route exact path="/album/:id" component={ Album } />
       </Switch>
     );
   }
