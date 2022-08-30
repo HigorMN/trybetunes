321c991f94b1d6c63d08d2674ad187267f161157
Higor Maranhão <higor.maranhao2000@gmail.com>
2022-08-30 15:08:18 -0300

    Pages search criado

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React from 'react';
-//
+import { Route, Switch } from 'react-router-dom';
+import Login from './pages/Login';
+import Search from './pages/Search';
+
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunes</p>);
+    return (
+      <Switch>
+        <Route exact path="/" component={ Login } />
+        <Route exact path="/search" component={ Search } />
+      </Switch>
+    );
   }
 }
 
