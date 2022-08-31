e14b6ffd044205a3436350f6b6010cb1d451a8f0
Higor Maranhão <higor.maranhao2000@gmail.com>
2022-08-31 15:43:57 -0300

    requisito 12 concluido

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import Carregando from '../components/Carregando';
-import { getFavoriteSongs } from '../services/favoriteSongsAPI';
-import MusicCard from '../components/MusicCard';
+import { getFavoriteSongs, removeSong } from '../services/favoriteSongsAPI';
 
 export default class Favorites extends Component {
   state = {
@@ -20,6 +19,17 @@ export default class Favorites extends Component {
     this.setState({ loading: false, musicas: fetch, favoritos: filtro });
   };
 
+  handleChange = async ({ target }) => {
+    const { value } = target;
+    this.setState({ loading: true });
+    const fetch = await getFavoriteSongs();
+    const filtro = fetch.find((e) => e.trackId === Number(value));
+    await removeSong(filtro);
+    const novo = await getFavoriteSongs();
+    const find = novo.map((e) => e.trackId);
+    this.setState({ loading: false, musicas: novo, favoritos: find });
+  };
+
   render() {
     const { loading, musicas, favoritos } = this.state;
     return (
@@ -29,13 +39,24 @@ export default class Favorites extends Component {
             <ul>
               {musicas.map((e, index) => (
                 <li key={ index }>
-                  <MusicCard
-                    trackName={ e.trackName }
-                    previewUrl={ e.previewUrl }
-                    trackId={ e.trackId }
-                    objAPI={ musicas }
-                    checked={ favoritos.find((i) => (i === e.trackId)) }
-                  />
+                  <h2>{e.trackName}</h2>
+                  <audio data-testid="audio-component" src={ e.previewUrl } controls>
+                    <track kind="captions" />
+                  </audio>
+                  <label
+                    htmlFor={ e.trackId }
+                    data-testid={ `checkbox-music-${e.trackId}` }
+                  >
+                    Favorita
+                    <input
+                      type="checkbox"
+                      value={ e.trackId }
+                      name="favorito"
+                      id={ e.trackId }
+                      checked={ favoritos.find((i) => (i === e.trackId)) }
+                      onChange={ this.handleChange }
+                    />
+                  </label>
                 </li>
               ))}
             </ul>
