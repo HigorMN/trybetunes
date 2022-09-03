47f059fdd091afffaf89ff90e8850e9715e5385c
Higor Maranhão <higor.maranhao2000@gmail.com>
2022-09-03 17:40:31 -0300

    css header user feito

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,10 +3,12 @@ import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 import Carregando from './Carregando';
 import logoWhite from '../images/logoWhite.png';
+import userImg from '../images/userImg.png';
 
 export default class Header extends Component {
   state = {
     user: '',
+    userImgl: '',
     loading: true,
   };
 
@@ -16,16 +18,30 @@ export default class Header extends Component {
 
   fetchAPI = async () => {
     const fetchAPI = await getUser();
-    this.setState({ user: fetchAPI.name, loading: false });
+    const validImage = fetchAPI.image.length > 0 ? fetchAPI.image : userImg;
+    this.setState({
+      user: fetchAPI.name,
+      userImgl: validImage,
+      loading: false,
+    });
   };
 
   render() {
-    const { user, loading } = this.state;
+    const { user, loading, userImgl } = this.state;
     return (
       <header data-testid="header-component">
-        <div>
+        <div className="conteiner-header1">
           <img src={ logoWhite } alt="logo trybe tunes" />
-          {loading ? <Carregando /> : <h1 data-testid="header-user-name">{user}</h1>}
+          {loading
+            ? <Carregando />
+            : (
+              <div className="user-icon">
+                <img src={ userImgl } alt="" className="header-user-image" />
+                <h2 data-testid="header-user-name" className="header-user-name">
+                  {user}
+                </h2>
+                <div />
+              </div>)}
         </div>
 
         <nav>
