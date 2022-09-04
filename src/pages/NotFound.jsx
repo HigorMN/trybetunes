bf49bb4b3526dd91f5bd990fd2cf3d6a78e8769f
Higor Maranhão <higor.maranhao2000@gmail.com>
2022-09-04 17:46:54 -0300

    css concluido

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,9 +1,28 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import logoDark from '../images/logoDark.png';
 
 export default class NotFound extends Component {
   render() {
     return (
-      <div data-testid="page-not-found">NotFound</div>
+      <div data-testid="page-not-found" className="not-found">
+        <img
+          src={ logoDark }
+          alt="Logo trybe tunes"
+          className="image-login"
+        />
+        <div>
+          <h1>Ops!</h1>
+          <p>
+            A página que você
+            está procurando
+            não foi encontrada.
+            {' '}
+            <Link to="/search">voltar para pagina</Link>
+          </p>
+
+        </div>
+      </div>
     );
   }
 }
