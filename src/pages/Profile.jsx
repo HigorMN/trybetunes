0dc44f2babfaf1c90b4a1f5d57dfd94795c52505
Higor Maranhão <higor.maranhao2000@gmail.com>
2022-09-04 15:59:42 -0300

    profile css feito

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -18,22 +18,34 @@ export default class Profile extends Component {
   fetchUser = async () => {
     const fetch = await getUser();
     const validImage = fetch.image.length > 0 ? fetch.image : userImg;
-    this.setState({ loading: false, info: fetch, userImgl: validImage });
+    this.setState({
+      loading: false,
+      info: fetch,
+      userEmail: fetch.email.length > 0 ? fetch.email : 'usuario@usuario.com.br',
+      userImgl: validImage,
+    });
   };
 
   render() {
-    const { loading, info, userImgl } = this.state;
+    const { loading, info, userImgl, userEmail } = this.state;
     return (
       <div data-testid="page-profile">
         {loading
           ? <Carregando />
           : (
             <div className="container-profile">
-              <img src={ userImgl } alt={ info.name } data-testid="profile-image" />
-              <Link to="/profile/edit">Editar perfil</Link>
-              <p>{info.name}</p>
-              <p>{info.email}</p>
-              <p>{info.description}</p>
+              <div className="img-link">
+                <img src={ userImgl } alt={ info.name } data-testid="profile-image" />
+                <Link to="/profile/edit">Editar perfil</Link>
+              </div>
+              <div className="container-name">
+                <h2>Nome</h2>
+                <p>{info.name}</p>
+                <h2>Email</h2>
+                <p>{userEmail}</p>
+                <h2>Descricao</h2>
+                <p>{info.description}</p>
+              </div>
             </div>)}
       </div>
     );
